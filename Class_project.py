1fd4bb933aae3cd6032df0acb540fc56f8a9a096
root <root@e-bibgbi-7xncx-0.e-bibgbi-7xncx.ace-hub.svc.cluster.local>
2023-05-28 21:52:21 +0000

    updating

--- Class_project.py
+++ Class_project.py
@@ -1,11 +1,12 @@
 #https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
 
+import itertools
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 from collections import Counter
@@ -21,6 +22,53 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 fig_path = '/root/CSCE623_Project/figures'
 
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    updated_classes = []
+    x_labels =[]
+    for idx, x in enumerate(classes):
+        update_string = x
+        update_string = update_string + ': '
+        update_string = update_string + str(idx)
+        updated_classes.append(update_string)
+        x_labels.append(str(idx))
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    #plt.xticks(tick_marks, classes, rotation=90, fontsize=8)
+    plt.xticks(tick_marks, x_labels, fontsize = 8)
+   # plt.xticks(tick_marks, classes, rotation=45, fontsize=2)
+    plt.yticks(tick_marks, updated_classes, fontsize=8)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), fontsize='x-small',
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig(f'{fig_path}/{title}.png')
+
 # def confusion_matrix(y_data, predicted_data):
 #     confusion_matrix = metrics.confusion_matrix(y_validation, predicted)
 #     df_cm = pd.DataFrame(confusion_matrix, range(20), range(20))
@@ -30,19 +78,17 @@ fig_path = '/root/CSCE623_Project/figures'
 
 # Note - should remove headers footers and quotes, as machine learning algorithms tend to focus on these
 #https://scikit-learn.org/0.19/datasets/twenty_newsgroups.html
-twenty_train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'), shuffle=True)
-twenty_test = fetch_20newsgroups(subset='test', remove=('headers', 'footers', 'quotes'), shuffle=True)
+# twenty_train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'), shuffle=True)
+# twenty_test = fetch_20newsgroups(subset='test', remove=('headers', 'footers', 'quotes'), shuffle=True)
+
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+
+# categories will be used for confusion matrix
+categories = twenty_train.target_names
 
 labels = ['1: atheism', '2: christian', '3: space', '4: politics', '5: religion', '6: autos', '7: windows', '8: mideast', '9: crypt', '10: motocycles', '11: graphics', '12: ibm-hardware',
           '13: hardware', '14: electronics', '15: forsale', '16: sci.med', '17: windows.', '18: baseball', '19: gun', '20: hockey']
-
-print(Counter(twenty_train.target).keys(), " ", Counter(twenty_train.target_names).keys())
-#print(twenty_train.filenames.shape)
-#print(twenty_train)
-
-
-#count_dict = dict(Counter(twenty_train.target).items())
-#print(count_dict)
 
 # The proportions of values in the training data set is as follows. No data set is underrepresented
 # 0: 480, 1: 584, 2: 591, 3: 590, 4: 578, 
@@ -56,24 +102,15 @@ y = twenty_train.target
 
 X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size = 0.2, random_state = 42)
 count_dict = dict(Counter(y_train).items())
-print("Training data: ")
-print(SortedDict(count_dict))
-
-
-
 # print("Training data: ")
-# print(Counter(y_train).keys())
+# print(SortedDict(count_dict))
+
 count_dict_val = dict(Counter(y_validation).items())
-print("Validation data: ")
-print(SortedDict(count_dict_val))
-# count_dict_test = dict(Counter(y_validation).items())
+# print("Validation data: ")
+# print(SortedDict(count_dict_val))
 
-print("Test data: ")
-# count_test = dict(Counter(twenty_test.target).keys())
-# print(SortedDict(count_test))
-print(SortedDict(Counter(twenty_test.target).items()))
-
-#print(Counter(twenty_test.target).values())
+# print("Test data: ")
+# print(SortedDict(Counter(twenty_test.target).items()))
 
 # print ("There are ", len(X_train), " measurements in the training database")
 # print ("There are ", len(X_validation), " measurements in the validation database")
@@ -83,17 +120,7 @@ print(SortedDict(Counter(twenty_test.target).items()))
 # 10: 492, 11: 470, 12: 477, 13: 475, 14: 466,
 # 15: 477, 16: 425, 17: 462, 18: 358, 19: 302,
 
-#count_dict = dict(Counter(y_validation).items())
-#print(count_dict)
-
-#print(twenty_train.len)
-#print(twenty_validation.len)
-
 # First do some preliminary analysis to make sure that training and test data categories are evenly distributed
-
-#print (len(twenty_test.data))
-#print(len(twenty_train.data))
-#print(twenty_train.target_names)
 #There are 7532 measurements in the test set
 #There are 11314 measurements in the training set
 
@@ -147,7 +174,7 @@ if bag_of_words:
     count_vect = CountVectorizer(strip_accents='unicode', min_df = 50, max_df = 400, max_features = 10000, ngram_range=(1, 3))
     X_train_counts = count_vect.fit_transform(twenty_train.data)
     #print(count_vect.vocabulary_)
-    print(X_train_counts.shape)
+    #print(X_train_counts.shape)
 
     #word_count_plot(X_train_counts)
 
@@ -161,15 +188,9 @@ if bag_of_words:
     # # print(count_vect.vocabulary_.get(u'alpha'))
 
     # print("the second word appears: ", X_train_counts.shape[1], " times")
-    
-    
-    
-    #array_names = ['god', 'guns', 'united', 'schools']
-    #print(count_vect.get_feature_names_out())
-    #print(X_train_counts.toarray())
     # CountVectorizer just turns articles into features with frequencies of words
-    # print(X_train_counts.shape)
-    #print(count_vect.vocabulary_)
+
+
 
     #bow_nb = MultinomialNB().fit(X_train_counts, y_train)
 
@@ -192,7 +213,13 @@ if bag_of_words:
     # Term frequency divides each word in a document by the total words in that document
     # Inverse document frequency decreases the weights of words based on how often they appear in many documents
 
+run_experiment_bool = True
 tfidf_bool = True
+naive_bayes_bool = False
+svm_bool = True
+random_forest_bool = False
+
+
 if tfidf_bool:
     # count_vect = CountVectorizer()
     # X_train_counts = count_vect.fit_transform(twenty_train.data)
@@ -201,13 +228,13 @@ if tfidf_bool:
     # print(tfidf_transformer.vocabulary_)
 
     tf_idf_vectorizer = TfidfVectorizer(max_features = 100, max_df = 1000, min_df = 50)
-    tf_idf_counts = tf_idf_vectorizer.fit_transform(twenty_train.data)
+    tf_idf_counts = tf_idf_vectorizer.fit_transform(X_train)
 
     feature_names = tf_idf_vectorizer.get_feature_names_out()
     dense = tf_idf_counts.todense()
     lst1 = dense.tolist()
     df = pd.DataFrame(lst1, columns=feature_names)
-    print(df.head)
+    #print(df.head)
     x = tf_idf_vectorizer.vocabulary_
     Cloud = WordCloud(background_color="white").generate_from_frequencies(df.T.sum(axis=1))
     Cloud.to_file(f'{fig_path}/tf_idf_cloud.png')
@@ -232,77 +259,45 @@ if tfidf_bool:
     # plt.title("TF-IDF and Multinomial Naive Bayes")
     # plt.show()
 
+    if naive_bayes_bool:
 
-    # # Try out the model on some new data
-    # docs_new = ['God is love', 'Cpus, computers, digital stuff, and best buy, I love computers and cpus so much']
-    # X_new_counts = count_vect.transform(docs_new)
-    # X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-    # predicted = clf.predict(X_new_tfidf)
+        # Create a pipeline that performs the three relevant functions
+        text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB()),])
+        text_clf.fit(X_train, y_train)
+        predicted = text_clf.predict(X_validation)
+        # The predicted value on the tf-idf validation set is 0.844
+        #print(np.mean(predicted == y_validation))
+        
+        conf_matrix = confusion_matrix(y_validation, predicted)
+        plot_confusion_matrix(conf_matrix, categories, title = 'TF-IDF Naive Bayes Confusion Matrix')
 
-    # for doc, category in zip(docs_new, predicted):
-    #     print('%r => %s' % (doc, twenty_train.target_names[category]))
+        print (metrics.classification_report(y_validation, predicted))
 
-    # Create a pipeline that performs the three relevant functions
-    text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB()),])
-    text_clf.fit(X_train, y_train)
-    predicted = text_clf.predict(X_validation)
-    # The predicted value on the tf-idf validation set is 0.844
-    #print(np.mean(predicted == y_validation))
-    
-    conf_matrix = confusion_matrix(y_validation, predicted)
-    vis = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=text_clf.classes_)
-    
-    #vis.set_title("Bag_of_words and Multinomial Naive Bayes")
-    vis.plot()
-    plt.title("TF-IDF and Multinomial Naive Bayes")
-    # ax = plt.add_subplot()
-    # ax.set_xticklabels([''] + labels)
-    plt.show()
+    if svm_bool:
+        text_clf = Pipeline([('vect', CountVectorizer(ngram_range = (1,3), min_df = 10)), ('tfidf', TfidfTransformer()), ('clf', SGDClassifier(loss = 'hinge', penalty = 'l2',
+                                                                                                            alpha=1e-3, random_state = 42,
+                                                                                                            max_iter = 5, tol=None)),])
+        
+        text_clf.fit(X_train, y_train)
 
-    print (metrics.classification_report(y_validation, predicted))
+        if run_experiment_bool:
+            print("Inside experiment loop")
+            parameters = { 'vect__ngram_range': [(1, 1), (1, 2), (1, 3)],
+            'clf__alpha': (1e-2, 1e-3, 1e-4, 1e-5),}
+            gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
+            gs_clf = gs_clf.fit(twenty_train.data, twenty_train.target)
+            print(gs_clf.best_score_)
+            print(gs_clf.best_params_)
 
-    print("start SVM section: ")
-    text_clf = Pipeline([('vect', CountVectorizer(ngram_range = (1,3), min_df = 10)), ('tfidf', TfidfTransformer()), ('clf', SGDClassifier(loss = 'hinge', penalty = 'l2',
-                                                                                                        alpha=1e-3, random_state = 42,
-                                                                                                        max_iter = 5, tol=None)),])
-    text_clf.fit(X_train, y_train)
-    predicted = text_clf.predict(X_validation)
-    # The predicted value of the tf-idf validation set is 0.891
-    print(np.mean(predicted == y_validation))
-    print (metrics.classification_report(y_validation, predicted))
 
-    #               precision    recall  f1-score   support
+        predicted = text_clf.predict(X_validation)
+        # The predicted value of the tf-idf validation set is 0.891
+        print("Prediction accuracy on validation set is: ", np.mean(predicted == y_validation))
+        print (metrics.classification_report(y_validation, predicted))
 
-    #            0       0.93      0.91      0.92        97
-    #            1       0.88      0.78      0.83       104
-    #            2       0.83      0.84      0.84       115
-    #            3       0.77      0.73      0.75       123
-    #            4       0.95      0.83      0.88       126
-    #            5       0.84      0.93      0.88       106
-    #            6       0.81      0.87      0.84       109
-    #            7       0.92      0.94      0.93       139
-    #            8       0.91      0.97      0.94       122
-    #            9       0.94      0.96      0.95       102
-    #           10       0.89      0.94      0.92       108
-    #           11       0.95      0.98      0.97       125
-    #           12       0.95      0.79      0.86       114
-    #           13       0.93      0.95      0.94       119
-    #           14       0.93      0.99      0.96       127
-    #           15       0.82      0.93      0.87       122
-    #           16       0.90      0.95      0.92       121
-    #           17       0.88      0.99      0.93       102
-    #           18       0.93      0.86      0.89       107
-    #           19       0.88      0.56      0.68        75
+        # Plot Confusion Matrix
+        confusion_matrix = metrics.confusion_matrix(y_validation, predicted)
+        plot_confusion_matrix(confusion_matrix, categories, title = "TF-IDF SVM Confusion Matrix")
 
-    #     accuracy                           0.89      2263
-    #    macro avg       0.89      0.89      0.89      2263
-    # weighted avg       0.89      0.89      0.89      2263
-
-    confusion_matrix = metrics.confusion_matrix(y_validation, predicted)
-    df_cm = pd.DataFrame(confusion_matrix, range(20), range(20))
-    sn.set(font_scale=1.4) # for label size
-    sn.heatmap(df_cm, annot=True, annot_kws={"size": 7}) # font size
-    plt.show()
-
-    #Maybe consider word2vec
-    # We can look at word2vec as future work. Or we can possibly take a model out of the box and start messing with it.
+        #Maybe consider word2vec
+        # We can look at word2vec as future work. Or we can possibly take a model out of the box and start messing with it.
